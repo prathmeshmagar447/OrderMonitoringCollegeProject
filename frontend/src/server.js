2769275db7f9a74165503fb979e6c87c378394c1
prathmeshmagar447 <prathmeshmagar447@gmail.com>
2024-11-04 17:34:00 +0530

    new
    new

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,6 @@
-export const server = "https://eshop-tutorial.vercel.app/api/v2";
+export const server = "http://localhost:8000/api/v2";
+
+export const backend_url = "http://localhost:8000/";
+
 
 
